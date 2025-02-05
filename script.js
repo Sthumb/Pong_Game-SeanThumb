ee0f02676b72af783fc70133a4d033780d81ed44
Sthumb <119553517+Sthumb@users.noreply.github.com>
2025-02-04 23:37:53 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Pong Game with Computer Paddle</title>
+  <style>
+    canvas {
+      background-color: black;
+      display: block;
+      margin: 0 auto;
+    }
+  </style>
+</head>
+<body>
+  <canvas id="pong" width="800" height="400"></canvas>
+  <script src="https://cdn.jsdelivr.net/npm/@tensorflow-models/handtrackjs"></script>
+  <script>
+    const canvas = document.getElementById("pong");
+    const ctx = canvas.getContext("2d");
+
+    // Rest of your Pong game code here...
+  </script>
+</body>
+</html>
